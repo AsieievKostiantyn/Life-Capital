a718720726d58295f7984ad7ff95940b4d9bc9f4
Kostia <aks20050504@gmail.com>
2026-02-04 22:53:01 +0200

    fix: route tabs ui

--- src/pages/player-expenses/layout/PlayerExpensesLayout.tsx
+++ src/pages/player-expenses/layout/PlayerExpensesLayout.tsx
@@ -1,22 +1,33 @@
-import { Outlet, useNavigate } from 'react-router-dom';
+import { Outlet, useLocation, useNavigate } from 'react-router-dom';
 
 import { Tabs } from '@mantine/core';
 
+import { GAME_ROUTES } from '@/shared/router';
+
 export const PlayerExpensesLayout = () => {
   const navigate = useNavigate();
+  const location = useLocation();
+
+  const activeTab = Object.values(
+    GAME_ROUTES.PLAYER_ROUTES.PLAYER_EXPENSES
+  ).find(
+    (route) =>
+      location.pathname.endsWith(`/${route}`) ??
+      GAME_ROUTES.PLAYER_ROUTES.PLAYER_EXPENSES.OVERVIEW
+  );
 
   return (
     <>
-      <Tabs defaultValue="overview" onChange={(value) => navigate(`${value}`)}>
+      <Tabs value={activeTab} onChange={(value) => navigate(`${value}`)}>
         <Tabs.List>
           <Tabs.Tab
-            value="overview"
+            value={GAME_ROUTES.PLAYER_ROUTES.PLAYER_EXPENSES.OVERVIEW}
             className="data-[active]:[&_.mantine-Tabs-tabLabel]:font-bold"
           >
             Загальні
           </Tabs.Tab>
           <Tabs.Tab
-            value="my"
+            value={GAME_ROUTES.PLAYER_ROUTES.PLAYER_EXPENSES.MY}
             className="data-[active]:[&_.mantine-Tabs-tabLabel]:font-bold"
           >
             Мої витрати
--- src/pages/player-investments/layout/PlayerInvestmentsLayout.tsx
+++ src/pages/player-investments/layout/PlayerInvestmentsLayout.tsx
@@ -1,22 +1,30 @@
-import { Outlet, useNavigate } from 'react-router-dom';
+import { Outlet, useLocation, useNavigate } from 'react-router-dom';
 
 import { Tabs } from '@mantine/core';
 
+import { GAME_ROUTES } from '@/shared/router';
+
 export const PlayerInvestmentsLayout = () => {
   const navigate = useNavigate();
+  const location = useLocation();
+
+  const activeTab =
+    Object.values(GAME_ROUTES.PLAYER_ROUTES.PLAYER_INVESTMENTS).find((route) =>
+      location.pathname.endsWith(`/${route}`)
+    ) ?? GAME_ROUTES.PLAYER_ROUTES.PLAYER_INVESTMENTS.CURRENT;
 
   return (
     <>
-      <Tabs defaultValue="current" onChange={(value) => navigate(`${value}`)}>
+      <Tabs value={activeTab} onChange={(value) => navigate(`${value}`)}>
         <Tabs.List>
           <Tabs.Tab
-            value="current"
+            value={GAME_ROUTES.PLAYER_ROUTES.PLAYER_INVESTMENTS.CURRENT}
             className="data-[active]:[&_.mantine-Tabs-tabLabel]:font-bold"
           >
             Поточні угоди
           </Tabs.Tab>
           <Tabs.Tab
-            value="my"
+            value={GAME_ROUTES.PLAYER_ROUTES.PLAYER_INVESTMENTS.MY}
             className="data-[active]:[&_.mantine-Tabs-tabLabel]:font-bold"
           >
             Мої інвестиції
